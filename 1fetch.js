ee06c1f75885b76ff65ed00f6e0c10c60d15910a
Harsh Negi <harshnegi0502@gmail.com>
2024-08-23 09:10:08 +0530

    Add files via upload

---
+++ 1fetch.js
@@ -0,0 +1,16 @@
+// fetch API --(application programming interface) used to fetching resources from the network
+
+
+/* syntax
+
+   fetch("http://example.com/api/endpoint")
+   .then((response){
+     // do dodmething with response
+   })
+   .catch(function(err){
+   console.log("unable to fetch",err);
+   });
+
+*/ 
+
+
